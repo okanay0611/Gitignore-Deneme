fff3f4180ed8001cb7480d3acf5e2e0585db3f84
Okan Ay <okanay0611@gmail.com>
2022-08-16 23:10:51 +0300

    versiyon 2

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -2,5 +2,6 @@ public class Gitignore {
     public static void main(String[] args) {
         System.out.println("Gitignore deneme 1");
         System.out.println("Gitignore deneme Brench");
+        System.out.println("Gitignore deneme master");
     }
 }
